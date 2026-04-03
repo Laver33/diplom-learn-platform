49d122824b1751f3a66c9bfd90e9f67f4eb1dd0a
Laver33 <pad666444@gmail.com>
2026-04-03 17:19:33 +0300

    feat: add Firebase, start create auth

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,19 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Конфигурация
 const firebaseConfig = {
-  apiKey: "AIzaSyCmfu0aN7E5foR7tpW3zmQXrmjVBZNdH9Q",
-  authDomain: "diplom-platform.firebaseapp.com",
-  projectId: "diplom-platform",
-  storageBucket: "diplom-platform.firebasestorage.app",
-  messagingSenderId: "716804061538",
-  appId: "1:716804061538:web:64b19d385820f2a207943b"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// инициализация
+// Инициализация Firebase
 const app = initializeApp(firebaseConfig);
-
 
 // Авторизация
 export const auth = getAuth(app);
